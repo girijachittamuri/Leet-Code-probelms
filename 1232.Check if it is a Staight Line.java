c23b6a518f3b79f51ce108d6a2a65ea8685b96b6
girijachittamuri <girijachittamuri@gmail.com>
2026-07-24 13:51:12 +0530

    1232.Check if it is a Staight Line.java

---
+++ 1232.Check if it is a Staight Line.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+
+        for (int i = 2; i < coordinates.length; i++) {
+            if ((coordinates[i][1] - coordinates[0][1]) *
+                (coordinates[1][0] - coordinates[0][0]) !=
+                (coordinates[1][1] - coordinates[0][1]) *
+                (coordinates[i][0] - coordinates[0][0])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
